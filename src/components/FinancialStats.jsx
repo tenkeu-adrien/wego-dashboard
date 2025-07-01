05faaac2290062aaf1b5b8681a683cb8a1313c15
Djeudje-Tenkeu <devagencyweb@gmail>
2025-07-01 07:51:38 +0100

    update .env && package

--- src/components/FinancialStats.jsx
+++ src/components/FinancialStats.jsx
@@ -10,7 +10,7 @@ const FinancialStats = () => {
   useEffect(() => {
     const fetchStats = async () => {
       try {
-        const response = await axios.get('http://localhost:3333/api/v1/dashboard/financial-stats');
+        const response = await axios.get('https://wegoadmin-c5c82e2c5d80.herokuapp.com/api/v1/dashboard/financial-stats');
         setStats(response.data.map(stat => ({
           ...stat,
           // Formatage des valeurs
--- src/configs/auth.config.js
+++ src/configs/auth.config.js
@@ -3,4 +3,4 @@
  * https://github.com/pinia-studio/jwt-api-node
 **/
 
-export const JWT_HOST_API = "http://localhost:3333/api/v1"
+export const JWT_HOST_API = "https://wegoadmin-c5c82e2c5d80.herokuapp.com/api/v1"
